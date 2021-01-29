6315c3f7f50c7e911bbf065a1f30e5fbebbe508f
rostia-vrt <vrostyk07@gmail.com>
2021-01-29 13:43:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Layout from '../src/hoc/Layout/Layout'
+
+function App() {
+  return (
+    <Layout>
+
+
+      <div style={{width: 400, border: '1px solid black'}}>
+        <h1>Layout works</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
