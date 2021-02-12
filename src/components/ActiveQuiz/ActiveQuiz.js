24c395cddfbfaa2d69c0522dc56ea9a77c4726a1
rostia-vrt <vrostyk07@gmail.com>
2021-02-12 16:07:28 +0200

    add new questions and styles

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import classes from '../ActiveQuiz/ActiveQuiz.module.css'
+import AnswersList from './AnswersList/AnswersList'
 
 
 const ActiveQuiz = props => (
     <div className={classes.ActiveQuiz}>
         <p className={classes.Question}>
             <span>
-                <strong>2.</strong>
-                &nbsp;How are you?
+                <strong>{props.answerNumber}.</strong>&nbsp;
+                {props.question}
             </span>
 
-            <small>4 from 12</small>
+            <small>{props.answerNumber} from {props.quizLength}</small>
         </p>
 
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+        <AnswersList
+            state={props.state}
+            answers={props.answers}
+            onAnswerClick={props.onAnswerClick}
+        />
     </div>
 )
 
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,7 +5,65 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
     state = {
-        quiz: []
+        activeQuestion: 0,
+        answerstate: null,
+        quiz: [
+            { question: 'Якого кольору небо?',
+                rightAnswerId: 2, 
+                id: 1,
+                answers: [
+                    {text: 'Чорний' , id: 1},
+                    {text: 'Синій', id: 2},
+                    {text: 'Зелений', id: 3},
+                    {text: 'Червоний', id: 4}
+                ]
+            },
+            { question: 'В котрому році заснували Львів?',
+                rightAnswerId: 4, 
+                id: 2,
+                answers: [
+                    {text: '1568' , id: 1},
+                    {text: '1517', id: 2},
+                    {text: '980', id: 3},
+                    {text: '1256', id: 4}
+                ]
+            }
+        ]
+    }
+
+    onAnswerClickHandler = answerId => {
+        const question = this.state.quiz[this.state.activeQuestion]
+
+        if(question.rightAnswerId === answerId){
+
+            this.setState({
+                answerState: {[answerId]: 'success'}
+            })
+
+            const timeOut = window.setTimeout(() => {
+                if(this.isQuizFinished()) {
+                    console.log('Finished');
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1
+                    })
+                }
+
+                window.clearTimeout(timeOut)
+            }, 1000)
+
+            
+        } else {
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            })
+        }
+
+        
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
 
@@ -13,9 +71,16 @@ class Quiz extends Component {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
-                    <h1>Quiz</h1>
+                    <h1>Дайте відповідь на запитання</h1>
 
-                    <ActiveQuiz/>
+                    <ActiveQuiz
+                    answers={this.state.quiz[this.state.activeQuestion].answers}
+                    question={this.state.quiz[this.state.activeQuestion].question}
+                    onAnswerClick={this.onAnswerClickHandler}
+                    quizLength={this.state.quiz.length}
+                    answerNumber={this.state.activeQuestion + 1}
+                    state={this.state.answerstate}
+                    />
                 </div>
             </div>
         )
